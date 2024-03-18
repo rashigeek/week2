f6d6865c563634938be7f6f1371da4a7b44bf07b
rashigeek <34960480+rashigeek@users.noreply.github.com>
2024-03-18 06:39:02 -0400

    adding assignment template

---
+++ starter.py
@@ -0,0 +1,23 @@
+# Simple Python code as a starter for a class on Python's variables, data types, and operators.
+
+# Variable assignments
+a = 5               # Integer
+b = 3.2             # Floating point number
+
+# Basic math operations
+sum_of_a_b = a + b  # Addition
+diff_of_a_b = a - b # Subtraction
+
+# 1. Uncomment and complete the lines below
+product_of_a_b = a*b
+#quotient_of_a_b = 
+
+
+
+print("\nOperations with Variables:")
+print(f"The sum of a and b is {sum_of_a_b}")
+print(f"The difference of a and b is {diff_of_a_b}")
+
+# 2. Uncomment the lines below aftre
+print(f"The product of a and b is {product_of_a_b}")
+#print(f"The quotient of a and b is {quotient_of_a_b}")
